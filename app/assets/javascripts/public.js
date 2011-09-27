216d855179ed1d7a3cbc735a0d51663f07898039
kingston <web@kingstontam.com>
2011-09-26 21:41:53 -0700

    Added component JS and added learn more button to Home and About

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -2,12 +2,5 @@
 
 var PublicIndexPage = {
   init: function() {
-    $('#learn-more-button').click(function() {
-      $('#sign-up-dialog').dialog({
-        height: 480,
-        width: 470,
-        modal: true
-      });
-    });
   }
 }
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,14 @@ module ApplicationHelper
   end
 
   def init_page_js(page_name)
+    init_component_js(page_name + "Page")
+  end
+
+  def init_component_js(component_name)
     content_for :js do
       %{
         <script type="text/javascript">
-          #{page_name}Page.init();
+          #{component_name}.init();
         </script>
       }.html_safe
     end
